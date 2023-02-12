51fcf191955027c1717c1d7933a714d3c74f8153
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2023-02-12 15:38:27 +0300

    Add files via upload

--- vector2.cpp
+++ vector2.cpp
@@ -30,10 +30,7 @@ void itc_rev_par_list(vector <int>& mass) {
 }
 
 void itc_rshift_list(vector <int>& mass) {
-    if (mass.size() == 0) {
-
-    }
-    else{
+    if (mass.size() > 0) {
         int b = mass[mass.size() - 1];
         for (int i = mass.size() - 1; i > 0; i--) {
             mass[i] = mass[i - 1];
